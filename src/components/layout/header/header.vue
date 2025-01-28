34a016c74d68b40e9dafa0013a8f6d3bbf4e87c5
Евгений <skytrash118@gmail.com>
2025-01-28 20:52:12 +0400

    header

--- src/components/layout/header/header.vue
+++ src/components/layout/header/header.vue
@@ -2,7 +2,7 @@
 </script>
 <template>
   <header class="header">
-    <article class="heder__up">
+    <article class="header__up">
       <div class="header__up__city">
         <a href="/Work">
           <h1>Работа в Борисоглебске</h1>
@@ -15,7 +15,18 @@
         </a>
       </div>
       <div class="header__up__site">
-
+        <a href="#">
+          <div class="header__up__site__searth">
+            <img src="../../../../public/header/search.svg" alt="">
+            <h1>Поиск по сайту</h1>
+          </div>
+        </a>
+        <a href="/Auth">
+          <div class="header__up__site__auth">
+            <img src="../../../../public/header/login.svg" alt="">
+            <h1>Вход</h1>
+          </div>
+        </a>
       </div>
     </article>
     <article class="header__logo">
@@ -27,5 +38,5 @@
   </header>
 </template>
 <style lang="scss" scoped>
-  @import "./header.scss";
+@import "./header.scss";
 </style>
