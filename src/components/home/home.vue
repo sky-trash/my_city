6cb67f2d9d72225d13c0d03eef3674f167980c87
Евгений <skytrash118@gmail.com>
2025-02-06 22:55:48 +0400

    home navbar

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,7 +1,26 @@
 <script setup>
 </script>
 <template>
+  <main class="main">
+    <article class="main__news">
+      <div class="main__news__navbar">
+        <div class="main__news__navbar__all">
+          <h1>Все новости</h1>
+        </div>
+        <div class="main__news__navbar__more">
+          <a href="/News">
+            <h1>Подробнее</h1>
+            <img src="../../../public/home/right.svg" alt="">
+          </a>
+        </div>
+      </div>
+      <div class="main__news__content">
+        <div>
 
+        </div>
+      </div> 
+    </article>
+  </main>
 </template>
 <style scoped>
   @import "./home.scss";
