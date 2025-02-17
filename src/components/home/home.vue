4aa932e4d310f2f373d3f8634751ed30d8a5c683
Евгений <skytrash118@gmail.com>
2025-02-18 01:02:22 +0400

    home cause-five

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -18,24 +18,49 @@
         <div>
 
         </div>
-      </div> 
+      </div>
     </article>
     <article class="main__cause">
-      <img src="../../../public/home/cause.svg" alt="">
-      <div class="main__cause__text">
-        <h1>5 весомых причин переехать в Борисоглебск</h1>
+      <img id="image-cause" src="../../../public/home/cause.svg" alt="">
+      <div class="main__cause__content">
+        <div class="main__cause__content__text">
+          <h1>5 весомых причин<br> переехать в Борисоглебск</h1>
+        </div>
+        <div class="main__cause__content__five">
+          <div class="main__cause__content__five__cause">
+            <img src="../../../public/home/story.svg" alt="">
+            <h1>Историчесий город</h1>
+          </div>
+          <div class="main__cause__content__five__cause">
+            <img src="../../../public/home/work.svg" alt="">
+            <h1>Возможность трудоустройства</h1>
+          </div>
+          <div class="main__cause__content__five__cause">
+            <img src="../../../public/home/life.svg" alt="">
+            <h1>Жилье</h1>
+          </div>
+          <div class="main__cause__content__five__cause">
+            <img src="../../../public/home/rest.svg" alt="">
+            <h1>Возможность для отдыха</h1>
+          </div>
+          <div class="main__cause__content__five__cause">
+            <img src="../../../public/home/children.svg" alt="">
+            <h1>Объекты для детей</h1>
+          </div>
+        </div>
+        <div class="main__cause__content__button">
+          <button>
+            <h1>Подробнее</h1>
+            <img src="../../../public/home/right-cause.svg" alt="">
+          </button>
+        </div>
       </div>
-      <div class="main__cause__">
+    </article>
+    <article class="main__map">
 
-      </div>
-      <div class="main__cause__button">
-        <button>
-          
-        </button>
-      </div>
     </article>
   </main>
 </template>
 <style scoped>
-  @import "./home.scss";
+@import "./home.scss";
 </style>
