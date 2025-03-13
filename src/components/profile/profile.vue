7867f3552967d7dd177146383a6fe909ddb29661
Евгений <skytrash118@gmail.com>
2025-03-13 12:23:36 +0400

    profile вуыскшзешщт

--- src/components/profile/profile.vue
+++ src/components/profile/profile.vue
@@ -14,16 +14,24 @@ import Footer from '../layout/footer/footer.vue';
     <article class="profile__description">
       <div class="profile__description__data">
         <div class="profile__description__data__name">
-
+          <h1>Евгений M.</h1>
         </div>
         <div class="profile__description__data__login">
-
+          <p>qwerty</p>
         </div>
         <div class="profile__description__data__email">
-
+          <p>qwerty123@gmail.com</p>
         </div>
       </div>
+      <div class="profile__description__change">
+        <button>
+          <h1>Изменить</h1>
+        </button>
+      </div>
     </article>
+    <div class="profile__line">
+      <img src="../../../public/profile/Line.svg" alt="">
+    </div>
     <article class="profile__history">
       <div>
 
