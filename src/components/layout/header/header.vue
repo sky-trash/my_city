2c894f57defeed423ce9a6f9e77fa35fe8f3dada
Евгений <skytrash118@gmail.com>
2025-03-19 15:30:10 +0400

    poster layout

--- src/components/layout/header/header.vue
+++ src/components/layout/header/header.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 </script>
 <template>
   <header class="header">
--- src/components/poster/poster.vue
+++ src/components/poster/poster.vue
@@ -1,16 +1,32 @@
 <script setup>
-  import Header from '../layout/header/header.vue';
-  import Application from '../application/application.vue';
-  import Footer from '../layout/footer/footer.vue';
+import Header from '../layout/header/header.vue';
+import Application from '../application/application.vue';
+import Footer from '../layout/footer/footer.vue';
 </script>
 <template>
-  <Header/>
+  <Header />
   <main class="poster">
-    
+    <article class="poster__layout">
+      <a href="/">
+        <div class="poster__layout__home">
+          <h1>Главная</h1>
+          <img src="../../../public/poster/right.svg" alt="">
+        </div>
+      </a>
+      <div class="poster__layout__text">
+        <h1>Афиша</h1>
+      </div>
+    </article>
+    <article class="poster__events">
+
+    </article>
+    <article class="poster__calendar">
+
+    </article>
   </main>
-  <Application/>
-  <Footer/>
+  <Application />
+  <Footer />
 </template>
 <style scoped>
-  @import "./poster.scss";
+@import "./poster.scss";
 </style>
