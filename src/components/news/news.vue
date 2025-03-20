2d506ff6d00bc501a2ce8c92b796a3f3a7a934d5
Евгений <skytrash118@gmail.com>
2025-03-20 22:09:55 +0400

    news frontend full

--- src/components/news/news.vue
+++ src/components/news/news.vue
@@ -1,10 +1,10 @@
 <script setup>
-  import Header from '../layout/header/header.vue';
-  import Application from '../application/application.vue';
-  import Footer from '../layout/footer/footer.vue';
+import Header from '../layout/header/header.vue';
+import Application from '../application/application.vue';
+import Footer from '../layout/footer/footer.vue';
 </script>
 <template>
-  <Header/>
+  <Header />
   <main class="news">
     <article class="news__layout">
       <a href="/">
@@ -18,12 +18,201 @@
       </div>
     </article>
     <article class="news__events">
-
+      <div class="news__events__text">
+        <h1>Все события</h1>
+      </div>
+      <div class="news__events__content">
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events1.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Происшествия</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>В Воронежской области потерпел крушение молет Су-34</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events2.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>История одного города</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>История Теллермаовского леса. Часть 1</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events2.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>История одного города</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>История Теллермаовского леса. Часть 2</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events3.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Спорт</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>Борисоглебские спортсменки в финале </h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events4.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Спорт</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>Нападающий ФК «Кристал-МЭ3» о матче с главными соперниками</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events5.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Общество</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>С Днём войск гражданской бороны МЧС России!</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events6.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Культура</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>Борисоглебский кадет выступит в Москвена гала - концерте</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events7.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Общество</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>Дошколята побывали гостях у пожарных!</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+        <a href="#">
+          <div class="news__events__content__card">
+            <div class="news__events__content__card__icons">
+              <img src="../../../public/news/events/events8.svg" alt="">
+            </div>
+            <div class="news__events__content__card__info">
+              <div class="news__events__content__card__info__tag">
+                <h1>Общество</h1>
+              </div>
+              <div class="news__events__content__card__info__text">
+                <h1>Подборка мероприятий на выходные</h1>
+              </div>
+              <div class="news__events__content__card__info__time">
+                <p>Вчера, 23:56</p>
+                <img src="../../../public/news/ear.svg" alt="">
+                <p>546</p>
+              </div>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div class="news__events__button">
+        <button>
+          <h1>Показать еще</h1>
+        </button>
+      </div>
     </article>
   </main>
-  <Application/>
-  <Footer/>
+  <Application />
+  <Footer />
 </template>
 <style>
-  @import "./news.scss";
+@import "./news.scss";
 </style>
